e41d177987ff4efc9eb8fa3393ec099d91953745
Kosui Iwasa <contact@ebiebievidence.com>
2019-05-20 17:44:15 +0900

    Add components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Game from './components/Game.vue'
+import Moles from './components/Moles.vue'
+import Mole from './components/Mole.vue'
+import Counter from './components/Counter.vue'
 
 Vue.config.productionTip = false
+
+Vue.component('game', Game)
+Vue.component('moles', Moles)
+Vue.component('mole', Mole)
+Vue.component('counter', Counter)
 
 new Vue({
   render: h => h(App),
