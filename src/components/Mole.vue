a09b694a30a0b71bab7a7673f9c425445480658f
Kosui Iwasa <contact@ebiebievidence.com>
2019-05-20 17:44:48 +0900

    Add mock mole

---
+++ src/components/Mole.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mole">
+    <img class="gopher" src="../assets/gopher.png" v-bind:style="gopherStyle">
+    <img class="dirt" src="../assets/dirt.svg">
+  </div>
+</template>
+
+<script>
+const initData = () => ({
+  gopherStyle: ""
+});
+
+export default {
+  name: "Mole",
+  props: ["mole"],
+  data: initData
+};
+</script>
+
+<style scoped>
+.mole {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  justify-content: flex-end;
+  height: 320px;
+  cursor: pointer;
+  overflow: hidden;
+}
+.mole .gopher {
+  position: absolute;
+  top: 100%;
+  transition: all 0.15s;
+  width: 60%;
+  z-index: -1;
+  transform: translateX(30%);
+}
+.mole .dirt {
+  width: 100%;
+}
+</style>
