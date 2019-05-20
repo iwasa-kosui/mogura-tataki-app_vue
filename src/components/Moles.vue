0a653e36f55ebcf4cd71510a7258b6c93bb41ad7
Kosui Iwasa <contact@ebiebievidence.com>
2019-05-20 17:44:52 +0900

    Add mock moles

---
+++ src/components/Moles.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="moles">
+    <mole
+      v-for="mole in $props.moles"
+      v-bind:mole="mole"
+      v-on:on-mole-click="$emit('on-mole-click', $event)"
+    ></mole>
+  </div>
+</template>
+
+<script>
+const initData = () => ({});
+
+export default {
+  name: "Moles",
+  props: ["moles"],
+  data: initData
+};
+</script>
+
+<style scoped>
+.moles {
+  display: flex;
+  justify-content: center;
+}
+</style>
