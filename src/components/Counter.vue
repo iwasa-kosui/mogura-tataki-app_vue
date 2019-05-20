d15eb2f3d4b700d748986c3f8925fadc658e4317
Kosui Iwasa <contact@ebiebievidence.com>
2019-05-20 17:44:38 +0900

    Add mock counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="counter"></div>
+</template>
+
+<script>
+const initData = () => ({});
+
+export default {
+  name: "Counter",
+  props: ["score", "highScore", "timeCount"],
+  data: initData
+};
+</script>
+
+<style scoped>
+</style>
